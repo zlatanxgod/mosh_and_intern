7c92217056f537d50fa49929cd318c5f8bc79b77
Abhishant Singh <abhishantsingh05@gmail.com>
2022-06-04 23:44:16 +0530

    basic api view, browasbale api

--- owner_detail/views.py
+++ owner_detail/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from owner_detail.models import Owner
 # Create your views here.
 
+@api_view()
 def say_hello(request):
-    query_set =  Owner.objects.all()
-    #q_object = Owner.objects.get(pk = 2)
-    q_object = Owner.objects.filter(pk=2).first()
+    # query_set =  Owner.objects.all()
+    # #q_object = Owner.objects.get(pk = 2)
+    # q_object = Owner.objects.filter(pk=2).first()
 
-    return HttpResponse("Hello")
+    return Response("Hello")
+
+@api_view()
+def owner_detail(request, id):
+    return Response(id)
